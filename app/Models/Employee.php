79ec4f7e2e467dc4b1e214f3d1ed9a54e370609b
parv shah <103427081+2003parvshah@users.noreply.github.com>
2025-04-10 23:43:26 +0530

    temp commit

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -1,10 +1,29 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'designation',
+        'profile_image',
+        'team', // example fields
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'employee_id');
+    }
 }
